db135cd091985d08375ad2e5ae2b9c20c53a8879
Nicola Szwaja <102081211+nicolaszwaja@users.noreply.github.com>
2023-04-04 22:04:57 +0200

    Add files via upload

---
+++ zadanie02/Segment.java
@@ -0,0 +1,36 @@
+public interface Segment {
+	/**
+	 * Kierunek rysowania odcinka. Kierunek ustalany jest poprzez wskazanie
+	 * inkrementacji/dekrementacji odpowiedniej współrzędnej tablicy reprezentującej
+	 * płótno, po którym rysujemy:
+	 * canvas[ pierwszy indeks ][ drugi_indeks ]
+	 * 
+	 * Obowiązuje następująca umowa:
+	 * <ul>
+	 * <li>1 - inkrementacja pierwszego indeksu tablicy
+	 * <li>2 - inkrementacja drugiego indeksu tablicy
+	 * <li>-1 - dekrementacja pierwszego indeksu tablicy
+	 * <li>-2 - dekrementacja drugiego indeksu tablicy
+	 * <li>Inne wartości są niedozwolone i jeżeli wystąpią odcinek nie powinien być
+	 * narysowany na płótnie
+	 * </ul>
+	 * 
+	 * @return liczba całkowita odpowiadająca kierunkowi rysowania
+	 */
+	public int getDirection();
+
+	/**
+	 * Metoda zwraca długość odcinka. Długość to liczba "pikseli", któłre należą do
+	 * odcinka.
+	 * 
+	 * @return liczba "pikseli" wchodzących w skład odcinka
+	 */
+	public int getLength();
+
+	/**
+	 * Metoda zwraca liczbę całkowitą reprezentującą kolor rysowanego odcinka.
+	 * 
+	 * @return kolor odcinka
+	 */
+	public int getColor();
+}
