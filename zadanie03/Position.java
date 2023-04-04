f11c65cd6d9157fca59e48db2b65d9d42fc2328c
Nicola Szwaja <102081211+nicolaszwaja@users.noreply.github.com>
2023-04-04 22:05:43 +0200

    Add files via upload

---
+++ zadanie03/Position.java
@@ -0,0 +1,18 @@
+
+/**
+ * PoĹoĹźenie w dwuwymiarowej przestrzeni
+ */
+public interface Position {
+	/**
+	 * WspĂłĹrzÄdna x
+	 * 
+	 * @return wspĂłĹrzÄdna x
+	 */
+	public int x();
+
+	/**
+	 * WspĂłĹrzÄdna y
+	 * @return wspĂłĹrzÄdna y
+	 */
+	public int y();
+}
---
+++ zadanie03/Position2D.java
@@ -0,0 +1,6 @@
+/**
+ * PoĹoĹźenie w dwĂłwymiarowej przestrzeni
+ *
+ */
+public record Position2D( int x, int y ) implements Position {
+}
